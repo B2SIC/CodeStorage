e411ba22d4cbad1ca76a96db75960e93d23d9505
BASIC <the_basic_@kookmin.ac.kr>
2022-11-22 01:02:32 +0900

    알고리즘 먼데이 7회 복습

--- Challenge/알고리즘먼데이/7회/이상한 미로.py
+++ Challenge/알고리즘먼데이/7회/이상한 미로.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
-# Unsolved
 import sys
 import heapq
 
@@ -8,34 +7,42 @@ INF = int(1e18)
 input = sys.stdin.readline
 
 n, m = map(int, input().rstrip().split())
-an_list = list(map(int, input().rstrip().split()))
+an_list = [0] + list(map(int, input().rstrip().split()))
 
 graph = [[] for _ in range(n + 1)]
-distance = [INF] * (n + 1)
+# distance = [INF] * (n + 1)
+distance = [[INF for _ in range(10)] for _ in range(n + 1)]
 
 for _ in range(m):
     u, v, w = map(int, input().rstrip().split())
     graph[u].append((v, w))
     graph[v].append((u, w))
 
+start = 1
+ans = -1
+q = []
+distance[start][0] = 0
+heapq.heappush(q, (distance[start][0], start, 0))
 
-def dijkstra(start):
-    q = []
-    distance[start] = 0
-    heapq.heappush(q, (0, start))
+while q:
+    dist, now, prev = heapq.heappop(q)
+    print(dist, now, prev)
+    print(distance[now][prev])
 
-    while q:
-        dist, now = heapq.heappop(q)
+    if distance[now][prev] < dist:
+        continue
 
-        if distance[now] < dist:
-            continue
+    if now == n:
+        ans = dist
+        break
 
-        for i in graph[now]:
+    for i in graph[now]:
+        if now == 1 or prev % an_list[now] == i[0] % an_list[now]:
+            # print(prev, '->', now, '->', i[0])
             cost = dist + i[1]
 
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+            if cost < distance[i[0]][now % an_list[i[0]]]:
+                distance[i[0]][now % an_list[i[0]]] = cost
+                heapq.heappush(q, (cost, i[0], now % an_list[i[0]]))
 
-dijkstra(1)
-print(distance)
+print(ans)

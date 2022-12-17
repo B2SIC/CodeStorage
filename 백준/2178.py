91cbcb5ad78614b705c8454560a3a31ec9cb95a2
BASIC <the_basic_@kookmin.ac.kr>
2022-12-18 01:09:45 +0900

    2022년 12월 17일 문제풀이 (복습)

--- 백준/2178.py
+++ 백준/2178.py
@@ -1,37 +1,36 @@
-import sys
 from collections import deque
 
-n, m = map(int, input().split())
+import sys
 
-graph = list()
-for i in range(n):
-    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+
+input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
+graph = []
+dist = [[-1] * m for _ in range(n)]
+
+for _ in range(n):
+    graph.append(list(map(int, input().rstrip())))
+
+queue = deque()
+queue.append((0, 0))
+dist[0][0] = 1
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+while queue:
+    x, y = queue.popleft()
 
-def dfs(x, y):
-    queue = deque()
-    queue.append((x, y))
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
 
-    while queue:
-        x, y = queue.popleft()
+        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            continue
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        if graph[nx][ny] and dist[nx][ny] == -1:
+            dist[nx][ny] = dist[x][y] + 1
+            queue.append((nx, ny))
 
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-
-    return graph[n - 1][m - 1]
-
-
-print(dfs(0, 0))
+print(dist[n - 1][m - 1])
